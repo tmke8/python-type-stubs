bc634ee82a7636e53c8accea8ebc28d0aaa6f776
Thomas M Kehrenberg <tmke8@posteo.net>
2025-07-24 15:34:59 +0200

    Make `config_context()` usuable as a context manager

--- stubs/sklearn/_config.pyi
+++ stubs/sklearn/_config.pyi
@@ -1,4 +1,5 @@
 from collections.abc import Iterator
+from contextlib import contextmanager
 from typing import Literal
 
 from ._typing import Int
@@ -19,6 +20,7 @@ def set_config(
     enable_metadata_routing: None | bool = None,
     skip_parameter_validation: None | bool = None,
 ) -> None: ...
+@contextmanager
 def config_context(
     *,
     assume_finite: None | bool = None,
